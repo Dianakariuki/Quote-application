b3b18dc08f2b77567877e0da5197c78148d50233
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-18 11:36:16 +0300

    This commit defines the title and subtitle component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
- 
+  title: string = 'QUOTE-APP';
+  subtitle: string = 'Create Your Quote';
   
   constructor() { }
 
