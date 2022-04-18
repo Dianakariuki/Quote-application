ad1eb4d372a394b41ba6e4bfaf2b255c36bf4c7b
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-18 11:48:55 +0300

    This commit declares the input onsubmit

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -9,6 +9,13 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 export class QuoteFormComponent implements OnInit {
   title: string = 'QUOTE-APP';
   subtitle: string = 'Create Your Quote';
+
+  onSubmit(): void  {
+    console.log(this.userInput);
+  }
+  userInput(userInput: any) {
+    throw new Error('Method not implemented.');
+  }
   
   constructor() { }
 
