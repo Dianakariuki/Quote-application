b78e24a2ba29633496823f5124d1a455cce08d79
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-19 01:19:59 +0300

    This commit creates a class quote.ts

---
+++ src/app/quote.ts
@@ -0,0 +1,20 @@
+export class Quote {
+  // quote class
+  id: number;
+  title: string;
+  author: string;
+  quote: string;
+  upvotes: number;
+  downvotes: number;
+  date: Date
+  // constructor
+  constructor(id: number, title: string, author: string, quote: string, upvotes: number, downvotes: number, date:Date) {
+      this.id = id;
+      this.title = title;
+      this.author = author;
+      this.quote = quote;
+      this.upvotes = upvotes;
+      this.downvotes = downvotes;
+      this.date = date
+  }
+}
